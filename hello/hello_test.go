b488924659aa7fbff9f738a084ea69caa99480cf
XiaoBeiAjie <20226790@stu.neu.edu.cn>
2025-10-22 15:55:34 +0800

    func(*): variable

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -2,8 +2,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("a_j1e")
+	want := "Hello, a_j1e"
 	
 	if got != want {
 		t.Errorf("got '%q' want '%q'", got, want)
