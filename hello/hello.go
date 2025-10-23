396df53d8044b9456d9c7e7cbc7561be34f27976
XiaoBeiAjie <20226790@stu.neu.edu.cn>
2025-10-23 13:14:34 +0800

    func(*): refator

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,12 @@
 package main
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
 func Hello(name string) string {
-	return "Hello, " + name
+	if name == "" {
+		name = "world"
+	}
+	return englishHelloPrefix + name
 }
 func main() {
 	fmt.Println(Hello("a_j1e"))
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,11 +1,24 @@
 package main
 import "testing"
 
+
 func TestHello(t *testing.T) {
-	got := Hello("a_j1e")
-	want := "Hello, a_j1e"
-	
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrentMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("a_j1e")
+		want := "Hello, a_j1e"
+		assertCorrentMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrentMessage(t, got, want)
+	})
 }
